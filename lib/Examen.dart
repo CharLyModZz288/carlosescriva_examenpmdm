3a94b8e3728ad8aaf3bc34254537ce8cd4d538f5
alumno1 <caesse280801@gmail.com>
2023-12-01 09:25:21 +0100

    PMDM:Creamos Examen.dart donde estaremos poniendo todas las clases

---
+++ lib/Examen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+class Examen extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    MaterialApp materialApp;
+    if(kIsWeb){
+      materialApp=MaterialApp(title: "Examen Carlos Escriva",
+
+
+      );
+    }else{
+      materialApp=MaterialApp(title: "Examen Carlos Escriva",
+
+
+      );
+    }
+
+
+  return MaterialApp();
+  }
+
+}
