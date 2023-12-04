58afbb39bc4d49419c512e82a738d7e36f81320c
alumno1 <caesse280801@gmail.com>
2023-12-04 09:06:09 +0100

    PMDM:P5 Agregamos la parte de psp para el menu lateral

---
+++ lib/Singletone/DataHolder.dart
@@ -0,0 +1,72 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import '../FirestoreObjects/FbPost.dart';
+import 'FirebaseAdmin.dart';
+
+
+class DataHolder {
+
+  static final DataHolder _dataHolder = DataHolder._internal();
+
+  String sNombre="Kyty DataHolder";
+  late String sPostTitle;
+  FbPost? selectedPost;
+  FirebaseFirestore db = FirebaseFirestore.instance;
+  FirebaseAdmin fbadmin=FirebaseAdmin();
+
+
+  DataHolder._internal() {
+
+  }
+
+  void initDataHolder(){
+
+    sPostTitle="Titulo de la publicacion";
+    //loadCachedFbPost();
+  }
+
+  factory DataHolder(){
+    return _dataHolder;
+  }
+
+  void insertPostEnFB(FbPost postNuevo){
+    CollectionReference<FbPost> postsRef = db.collection("Posts")
+        .withConverter(
+      fromFirestore: FbPost.fromFirestore,
+      toFirestore: (FbPost post, _) => post.toFirestore(),
+    );
+
+    postsRef.add(postNuevo);
+  }
+
+  void saveSelectedPostInCache() async{
+    if(selectedPost!=null){
+      SharedPreferences prefs = await SharedPreferences.getInstance();
+      prefs.setString('fbpost_titulo', selectedPost!.titulo);
+      prefs.setString('fbpost_cuerpo', selectedPost!.cuerpo);
+      prefs.setString('fbpost_surlimg', selectedPost!.sUrlImg);
+    }
+
+  }
+
+  Future<FbPost?> loadFbPost() async{
+    if(selectedPost!=null) return selectedPost;
+
+    await Future.delayed(Duration(seconds: 10));
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String? fbpost_titulo = prefs.getString('fbpost_titulo');
+    fbpost_titulo ??= "";
+    String? fbpost_cuerpo = prefs.getString('fbpost_cuerpo');
+    if(fbpost_cuerpo==null){
+      fbpost_cuerpo="";
+    }
+    String? fbpost_surlimg = prefs.getString('fbpost_surlimg');
+    if(fbpost_surlimg==null){
+      fbpost_surlimg="";
+    }
+    print("SHARED PREFERENCES!!!  ----->>>>> "+fbpost_titulo);
+    selectedPost=FbPost(titulo: fbpost_titulo, cuerpo: fbpost_cuerpo, sUrlImg: fbpost_surlimg);
+    return selectedPost;
+  }
+}
